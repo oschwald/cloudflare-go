a065d40c1fb8efdec08db8d83914cb7555532393
Gregory Oschwald <goschwald@maxmind.com>
2022-08-08 15:10:55 -0700

    Create separate tools module

--- tools/tools.go
+++ tools/tools.go
@@ -11,14 +11,27 @@ package tools
 //go:generate go install github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
 //go:generate go install github.com/go-delve/delve/cmd/dlv@latest
 //go:generate go install golang.org/x/lint/golint@latest
+//go:generate go install github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+//go:generate go install github.com/bflad/tfproviderlint/cmd/tfproviderlintx
+//go:generate go install github.com/client9/misspell/cmd/misspell
+//go:generate go install github.com/golangci/golangci-lint/cmd/golangci-lint
+//go:generate go install github.com/hashicorp/go-changelog/cmd/changelog-build
+//go:generate go install github.com/google/go-github/github
+//go:generate go install golang.org/x/oauth2
 
 import (
+	_ "github.com/bflad/tfproviderlint/cmd/tfproviderlintx"
+	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/cweill/gotests/gotests"
 	_ "github.com/go-delve/delve/cmd/dlv"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/google/go-github/github"
+	_ "github.com/hashicorp/go-changelog/cmd/changelog-build"
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 	_ "github.com/orijtech/structslop/cmd/structslop"
 	_ "github.com/ramya-rao-a/go-outline"
 	_ "github.com/uudashr/gopkgs/v2/cmd/gopkgs"
 	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/oauth2"
 	_ "golang.org/x/tools/gopls"
 )
